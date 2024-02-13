af4efc538f18858f901146b21feedfa68623e157
Dmitry <dmitry27zh@gmail.com>
2024-02-13 18:17:59 +0500

    Add extension

--- src/store/store.js
+++ src/store/store.js
@@ -2,7 +2,7 @@ import tasksReducer from './tasks'
 import { createStore } from 'redux'
 
 const configureStore = () => {
-  return createStore(tasksReducer)
+  return createStore(tasksReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 }
 
 export default configureStore
