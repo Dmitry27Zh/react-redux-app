7b517e0df4ab3188d92dca7e311e33d80638ecbf
Dmitry <dmitry27zh@gmail.com>
2024-02-13 10:06:45 +0500

    Add dispatch

--- src/index.js
+++ src/index.js
@@ -2,11 +2,24 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 const createStore = (initialState) => {
-  const state = initialState
+  let state = initialState
 
   return {
     getState() {
       return state
+    },
+    dispatch(action) {
+      if (action.type === 'task/completed') {
+        state = state.map((item) => {
+          if (item.id === action.payload.id) {
+            return { ...item, completed: true }
+          } else {
+            return item
+          }
+        })
+      }
+
+      console.log(state)
     },
   }
 }
@@ -14,7 +27,25 @@ const createStore = (initialState) => {
 const store = createStore([{ id: 1, description: 'Task 1', completed: false }])
 
 const App = () => {
-  return <h1>App</h1>
+  console.log(store.getState())
+
+  return (
+    <>
+      <h1>App</h1>
+      <button
+        onClick={() =>
+          store.dispatch({
+            type: 'task/completed',
+            payload: {
+              id: 1,
+            },
+          })
+        }
+      >
+        Complete
+      </button>
+    </>
+  )
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
