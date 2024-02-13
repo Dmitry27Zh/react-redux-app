075c132ba1407b8ca027ac18fcb335568cc14590
Dmitry <dmitry27zh@gmail.com>
2024-02-13 09:55:40 +0500

    Create Store

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+const createStore = (initialState) => {
+  const state = initialState
+
+  return {
+    getState() {
+      return state
+    },
+  }
+}
+
+const store = createStore([{ id: 1, description: 'Task 1', completed: false }])
+
 const App = () => {
   return <h1>App</h1>
 }
