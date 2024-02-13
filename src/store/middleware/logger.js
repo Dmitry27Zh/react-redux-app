328371846f9a1edc1012da3ca2450da6d422da20
Dmitry <dmitry27zh@gmail.com>
2024-02-13 18:55:17 +0500

    Create logger middleware

---
+++ src/store/middleware/logger.js
@@ -0,0 +1,11 @@
+export function logger(state) {
+  return function wrapDispatch(next) {
+    return function handleAction(action) {
+      console.log(state)
+      console.log(next)
+      console.log(action)
+
+      return next(action)
+    }
+  }
+}
--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,14 @@
+import { logger } from './middleware/logger'
 import tasksReducer from './tasks'
-import { createStore } from 'redux'
+import { createStore, compose, applyMiddleware } from 'redux'
+
+const middlewareEnhancer = applyMiddleware(logger)
 
 const configureStore = () => {
-  return createStore(tasksReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+  return createStore(
+    tasksReducer,
+    compose(window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(), middlewareEnhancer)
+  )
 }
 
 export default configureStore
