82cab716622acea476097566171085ca4f102df1
Dmitry <dmitry27zh@gmail.com>
2024-02-14 10:18:17 +0500

    Import react-redux

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import ReactDOM from 'react-dom/client'
 import createStore from './store/store'
 import { taskCompleted, taskRemoved, descriptionChanged, thunkDemo, getTasks } from './store/tasks'
+import { Provider } from 'react-redux'
 
 const store = createStore()
 
@@ -46,6 +47,8 @@ const App = () => {
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 )
